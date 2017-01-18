54191339ab5e8f9f77dcd36f265e1f6d97110605
Jarek Rencz <jarek.rencz@functionite.com>
2017-01-18 11:26:16 +0100

    move eslint-find-rules back to devDeps, use it only for this project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,19 @@
-module.exports = require('./');
+const index = require('./');
+
+const [
+  spellcheckLevel,
+  spellcheckOptions,
+] = index.rules['spellcheck/spell-checker'];
+
+module.exports = Object.assign({}, index, {
+  rules: Object.assign({}, index.rules, {
+    'spellcheck/spell-checker': [spellcheckLevel,
+      Object.assign({}, spellcheckOptions, {
+        skipWords: [
+          ...spellcheckOptions.skipWords,
+          ...require('eslint-find-rules')().getAllAvailableRules(),
+        ],
+      }),
+    ]
+  })
+});
